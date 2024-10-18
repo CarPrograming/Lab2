b15eb687db43105429fbcd5e26c1387cf37606ff
Carlos <carlosenm.03@gmail.com>
2024-10-18 13:29:59 -0400

    addind commetns to the code

--- js/page-color-changer.js
+++ js/page-color-changer.js
@@ -11,12 +11,13 @@ function changingBackgroud(){
     let greenValue = changeGreen.value;
     let blueValue = changeBlue.value;
 
+    //saving variables and set an string to use later to update color.
     let rndCol = 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')';
     
-
+    // changing the color
     document.documentElement.style.backgroundColor = rndCol;
 }
-
+// this are the event listened that call the fucntion after user slide the bar.
 changeRed.addEventListener('input',changingBackgroud);
 changeGreen.addEventListener('input',changingBackgroud);
 changeBlue.addEventListener('input',changingBackgroud);
